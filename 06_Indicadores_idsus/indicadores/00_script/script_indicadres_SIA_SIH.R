7f0a3c1b0eb8187716d746eb44fbf1f8ff3e23f4
danielppagotto <danielppagotto@gmail.com>
2022-07-14 21:50:59 -0300

    Update script_indicadres_SIA_SIH.R

--- 06_Indicadores_idsus/indicadores/00_script/script_indicadres_SIA_SIH.R
+++ 06_Indicadores_idsus/indicadores/00_script/script_indicadres_SIA_SIH.R
@@ -28,15 +28,34 @@ sia_pa <- sqlQuery(channel, consulta_sia)
 # Baixando dados - SIH
 
 consulta_sih <- 
-  "SELECT MUNIC_RES, COUNT(*) AS TOTAL 
+  "SELECT MUNIC_RES, DT_INTER, COUNT(*) AS TOTAL 
     FROM Dados.sih.RD
     WHERE (ANO_CMPT = '2020' OR ANO_CMPT = '2021') 
     AND (PROC_REA = '0201010151' OR 
-         PROC_REA = '0201010160' OR
+         PROC_REA = '0201010160'
          ) AND (COBRANCA = '11' OR
                 COBRANCA = '12') 
-    GROUP BY MUNIC_RES"
-
-# AND substr(PROC_REA, 1, 7)
+    GROUP BY MUNIC_RES, DT_INTER"
 
 sih <- sqlQuery(channel, consulta_sih)
+
+
+# Indicador 2 - Razão de internações clínico-cirúrgicas de média complexidade e população residente ----
+
+# Baixando dados - SIH - indicador 2
+
+consulta2_sih <- 
+  "SELECT MUNIC_RES, DT_INTER, COUNT(*) AS TOTAL 
+    FROM (SELECT MUNIC_RES, ANO_CMPT, CAST(PROC_REA AS INTEGER) AS PROC_REA_C, 
+                 COBRANCA
+                 FROM Dados.sih.RD)
+    WHERE (ANO_CMPT = '2020' OR ANO_CMPT = '2021') 
+    AND (
+            (PROC_REA_C >= 201010010 AND PROC_REA_C <= 201010159) OR
+            (PROC_REA_C >= 201010170 AND PROC_REA_C <= 201010579) 
+        )
+    AND (COBRANCA = '11' OR
+                COBRANCA = '12') 
+    GROUP BY MUNIC_RES, DT_INTER"
+
+# Érika - continuar preenchendo os intervalos de procedimentos conforme comecei a fazer aqui em cima
