f1325da284919595ebb5049f6b8953847a940f6f
Daniel Pagotto <danielppagotto@gmail.com>
2023-03-15 17:11:00 -0300

    UPDATE

--- 08_APS/coordenadas_gyn/tratamento_aps_gyn_coordenadas.R
+++ 08_APS/coordenadas_gyn/tratamento_aps_gyn_coordenadas.R
@@ -2,27 +2,28 @@
 library(tidyverse)
 library(readxl)
 
-equipes <- read_excel("coordenadas_gyn/equipes_bsb_gyn.xlsx")
+unidades_georreferenciadas <- read_excel("~/GitHub/dimensionamento/08_APS/bases/unidades_georreferenciadas.xlsx", 
+                                         col_types = c("text", "text", "text", 
+                                                       "numeric", "text", "text", "numeric", 
+                                                       "numeric", "numeric", "numeric", 
+                                                       "text", "numeric", "numeric", "numeric", 
+                                                       "numeric", "numeric", "numeric", 
+                                                       "numeric", "numeric", "numeric"))
+
+# BSB e GYN ---------------------------------------------------------------------
 
 
-# GYN ---------------------------------------------------------------------
-
-
-equipes_gyn <- equipes %>% 
-                  filter(municipio == "GOIÂNIA") %>% 
+equipes_gyn <- unidades_georreferenciadas %>% 
+                  filter(municipio == "GOIÂNIA" | municipio == "BRASÍLIA") %>% 
                   separate(col = coordenadas_geograficas, 
                            into = c('latitude','longitude'),
-                           sep = ',')
+                           sep = ',') %>% 
+                  filter(latitude != "NA")
+
+setwd("~/GitHub/dimensionamento/08_APS/base_PBI")
+
 
 # write.csv(equipes_gyn, 'equipes_gyn_tratado.csv')
-# writexl::write_xlsx(equipes_gyn, 'equipes_gyn_tratado.xlsx')
+ writexl::write_xlsx(equipes_gyn, 'equipes_gyn_tratado.xlsx')
 
 
-# BSB ---------------------------------------------------------------------
-
-
-equipes_bsb <- equipes %>% 
-                  filter(municipio == "BRASÍLIA") %>% 
-                  separate(col = coordenadas_geograficas, 
-                           into = c('latitude','longitude'),
-                           sep = ',')
