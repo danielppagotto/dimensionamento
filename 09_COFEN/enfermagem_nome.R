e74014eaf0109914ad493fe1002162e1f2c65fbd
Daniel Pagotto <danielppagotto@gmail.com>
2023-03-28 10:55:15 -0300

    Update enfermagem_nome.R

--- 09_COFEN/enfermagem_nome.R
+++ 09_COFEN/enfermagem_nome.R
@@ -3,9 +3,9 @@ library(dplyr)
 library(RODBC)
 library(tidyverse)
 
-dremio_host <- "200.137.215.27"
-dremio_port <- "31010"
-dremio_uid <- "daniel"
+dremio_host <- Sys.getenv('endereco')
+dremio_port <- Sys.getenv('port')
+dremio_uid <- Sys.getenv('uid')
 dremio_pwd <- Sys.getenv('datalake')
 
 channel <- odbcDriverConnect(sprintf("DRIVER=Dremio Connector;HOST=%s;PORT=%s;UID=%s;PWD=%s;AUTHENTICATIONTYPE=Basic Authentication;CONNECTIONTYPE=Direct", dremio_host, dremio_port, dremio_uid, dremio_pwd))
